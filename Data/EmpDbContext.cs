b28d1fd016d33d22b0ac77ae143faf78ada620b8
Afzal <130027973+af-zal@users.noreply.github.com>
2023-04-17 19:36:06 +0530

    EF changes

---
+++ Data/EmpDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmpCoreMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpCoreMVC.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmpCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
